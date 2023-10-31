72cdba9c1fdbfea656b9bdd2fd20213860e54e39
Karatsuban <raphael.garnier1919@gmail.com>
2023-10-31 14:09:28 +0100

    Now reading file in binary

--- File.py
+++ File.py
@@ -9,8 +9,9 @@ class File:
 		self.columns = int(columns)
 		print("HERE", lines, columns)
 		self.bufferSize = self.lines*self.columns # number of character displayed
-		self.bufferedHexContent = None # hexadecimal content
-		self.bufferedANContent = None # alphanumerical represenntation
+		self.bufferedContent = None # buffered file content
+		self.bufferedHex = None # hexadecimal content
+		self.bufferedAN = None # alphanumerical represenntation
 		self.currentFilePosition = None # file cursor
 
 		self.open()
@@ -18,10 +19,27 @@ class File:
 
 	def open(self):
 		# open the file and populates the buffers
-		self.file = open(self.filepath, "r+")
-		self.bufferedHexContent = self.file.read(4*self.bufferSize)
+		self.file = open(self.filepath, "rb+")
+		self.bufferedContent = self.file.read(4*self.bufferSize).hex()
 		self.currentFilePosition = 4*self.bufferSize # get the offset
+		self.setBuffs()
 
+	def setBuffs(self):
+		temp = [self.bufferedContent[k:k+2] for k in range(0, len(self.bufferedContent), 2)]
+		print("temp = ", temp)
+		self.bufferedHex = " ".join(temp)
+		self.bufferedAN = ""
+		for k in temp:
+			try:
+				char = bytes.fromhex(k).decode()
+			except UnicodeDecodeError:
+				char = "."
+			except ValueError:
+				print("Cant decode "+k)
+			print(k, end=" ")
+			self.bufferedAN += char
+		print()
+		#self.bufferedANContent = "".join([bytes.fromhex(k) for k in self.bufferedHexContent])
 
 	def close(self):
 		# close the file after use
@@ -32,4 +50,4 @@ class File:
 	# Getters
 
 	def getBuffers(self):
-		return self.bufferedHexContent, self.bufferedANContent
+		return self.bufferedHex, self.bufferedAN
