4d5904d7be89151386880354acaea86c627c987f
Karatsuban <raphael.garnier1919@gmail.com>
2023-11-01 21:36:43 +0100

    Simplified version, only managing a file, opening, reading and closing it

--- File.py
+++ File.py
@@ -2,42 +2,24 @@
 class File:
 
 
-	def __init__(self, filepath, lines, columns):
+	def __init__(self, filepath ):
 		self.filepath = filepath
 		self.file = None
-		self.lines = int(lines)
-		self.columns = int(columns)
-		self.bufferSize = self.lines*self.columns # number of character displayed
-		self.bufferedContent = None # buffered file content
-		self.bufferedHex = None # hexadecimal content
-		self.bufferedAN = None # alphanumerical represenntation
-		self.currentFilePosition = None # file cursor
-
-		self.open()
+		self.content = [] # file content
 
 
 	def open(self):
 		# open the file and populates the buffers
-		self.file = open(self.filepath, "rb+")
-		self.bufferedContent = self.file.read(4*self.bufferSize).hex()
-		self.currentFilePosition = 4*self.bufferSize # get the offset
-		self.setBuffs()
+		try :
+			self.file = open(self.filepath, "rb+")
+		except OSError:
+			return -1
+		else:
+			line = self.file.readline().hex()
+			while line != "":
+				self.content += [line[k:k+2] for k in range(0, len(line), 2)]
+				line = self.file.readline().hex()
 
-	def setBuffs(self):
-		temp = [self.bufferedContent[k:k+2] for k in range(0, len(self.bufferedContent), 2)]
-		self.bufferedHex = " ".join(temp)
-		self.bufferedAN = ""
-		for k in temp:
-			if (int(k, 16) <= 31):
-				char = "."
-			else:
-				try:
-					char = bytes.fromhex(k).decode()
-				except UnicodeDecodeError:
-					char = "."
-				except ValueError:
-					print("Cant decode "+k)
-			self.bufferedAN += char
 
 	def close(self):
 		# close the file after use
@@ -47,5 +29,5 @@ class File:
 
 	# Getters
 
-	def getBuffers(self):
-		return self.bufferedHex, self.bufferedAN
+	def getContent(self):
+		return self.content
