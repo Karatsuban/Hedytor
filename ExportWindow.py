4881dc6425aa4e856ad8aa75db62c17b63ceccc2
Karatsuban <raphael.garnier1919@gmail.com>
2023-11-04 14:41:04 +0100

    Added logic for exporting data in JSON

--- ExportWindow.py
+++ ExportWindow.py
@@ -1,15 +1,63 @@
-from PySide6.QtWidgets import QApplication, QTextEdit, QWidget
+from PySide6.QtWidgets import *
+import json
 
 class ExportWindow(QWidget):
 	
 	def __init__(self, data):
 		super().__init__()
-		layout = QVBoxLayout()
-		self.data = data
+		self.layout = QVBoxLayout()
+		self.data = data # data is a dictionary
+
+		print(self.data, type(self.data))
+		self.dataPrintable = ""
+		for key, val in self.data.items():
+			self.dataPrintable += str(key)+": "+str(val)+"\n"
 
 		self.dataDisplay = QTextEdit()
+		self.dataDisplay.setText(self.dataPrintable)
 
-		layout.addWidget(self.dataDisplay)
+		self.layout.addWidget(self.dataDisplay)
+
+		self.exportButton = QPushButton("Export to Json")
+		self.exportButton.clicked.connect(self.exportFromButton)
+		self.layout.addWidget(self.exportButton)
+
+		self.status = QStatusBar()
+		self.layout.addWidget(self.status)
+
 		self.setLayout(self.layout)
+
+	def exportFromButton(self):
+		dialog = QFileDialog(self)
+		dialog.AcceptMode(QFileDialog.AcceptSave) # set for save mode
+		dialog.setFileMode(QFileDialog.AnyFile)
+		dialog.setViewMode(QFileDialog.Detail)
+		if dialog.exec():
+			filenames = dialog.selectedFiles()
+		else:
+			filenames = []
+		if (len(filenames) != 0):
+			print("open from: "+", ".join(filenames))
+			self.exportToJSON(filenames[0])
+
+	def close(self):
+		self.dataDisplay.close()
+		super().close()
+
+	def exportToJSON(self, filepath):
+		print("In export to JSON, filename =", filepath)
+
+		jsoned_data = json.dumps(self.data)
+		print(jsoned_data)
+		
+		with open(filepath, "w") as file:
+			try:
+				json.dump(jsoned_data, file)
+			except:
+				self.status.showMessage("Error when writing!", 3000)
+			else:
+				self.status.showMessage("Write complete!", 3000)
+
+		
 
 
