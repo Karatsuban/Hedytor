fdd2df3b502b828650d95d9f8edf9b6e68b3de95
Karatsuban <raphael.garnier1919@gmail.com>
2023-11-05 22:02:22 +0100

    Cleaned up

--- PlainDelegate.py
+++ PlainDelegate.py
@@ -8,27 +8,36 @@ class PlainDelegate(QStyledItemDelegate):
 	printable_chars = string.ascii_letters + string.digits + string.punctuation + ' '
 	pr_id = [ord(k) for k in printable_chars]
 
-	def __init__(self, model, parent = None):
+
+	def __init__(self, parent = None):
 		super().__init__(parent)
-		self.model = model
 		self.parent = parent
-	
-	def displayText(self, value, locale):
+
+
+	def getRepr(self, value):
 		if int(value, 16) not in self.pr_id:
 			return "."
 		return bytes.fromhex(value).decode()
 
 
+	def displayText(self, value, locale):
+		return self.getRepr(value)
+
 	def createEditor(self, parent, option, index):
 		editor = QLineEdit(parent)
-		editor.setMaxLength(2) # only 2 characters
 		return editor
 
 
 	def setEditorData(self, editor, index):
+		# create the editor and set its value
 		value = index.data(Qt.DisplayRole)
+		editor.setText(self.getRepr(value))
 
 
 	def setModelData(self, editor, model, index):
-		value = bytes(editor.text(), 'utf-8').hex()
-		model.setData(index, value)
+		# get the new value from the editor and call the model to set it
+		valueFromEditor = editor.text()
+
+		valuesToInsert = "".join(valueFromEditor[::-1].encode().hex())
+		valuesToInsert = [valuesToInsert[k:k+2] for k in range(0, len(valuesToInsert), 2)]
+		model.insertData(index, valuesToInsert)
